617cb81bbd1746bb734bafd2eeed2f148bb49c03
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-03-20 12:05:28 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main() {
+    void print(void);
+    int square(int n);
 
+    int main() {
+    print();
+
+    int num = 5;
+    int result = square(num);
+    printf("%d %d\n", num, result);
+
+    return 0;
+}
+
+void print() {
+    printf("Welcome to Computer Programming 03 Lab!\n");
+}
+
+    int square(int n) {
+    return n * n;
+}
     return 0;
 }
