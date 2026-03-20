9b1240a645e9eba747ca399644c9af58976bab63
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-03-20 11:48:23 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
  int a = 10, b = 5;
-        printf("더하기: %d + %d = %d\n", a, b, a + b);
-    printf("빼기: %d - %d = %d\n", a, b, a - b);
-    printf("곱하기: %d * %d = %d\n", a, b, a * b);
-    printf("나누기: %d / %d = %d\n", a, b, a / b);
+        printf("%d + %d = %d\n" , a, b, a + b);
+    printf("%d - %d = %d\n", a, b, a - b);
+    printf("%d * %d = %d\n", a, b, a * b);
+    printf("%d / %d = %d\n", a, b, a / b);
+
 
     return 0;
 }
