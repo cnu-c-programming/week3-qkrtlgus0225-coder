dc4b10988884a528138626d8d821656f8ed80ae5
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-03-26 13:03:02 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int main() {
+int max_of(int count, ...) {
+    va_list args;
+    va_start(args, count);
 
+    int max = va_arg(args, int);
+
+    for (int i = 1; i < count; i++) {
+        int current = va_arg(args, int);
+        if (current > max) {
+            max = current;
+        }
+    }
+
+    va_end(args);
+    return max;
+}
+
+int main(){
+    printf("%d\n", max_of(4, 5, 12, 3, 9));       
+    printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6)); 
+    printf("%d\n", max_of(3, 100, 200, 300));    
+  
     return 0;
 }
