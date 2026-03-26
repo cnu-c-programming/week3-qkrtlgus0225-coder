c74c99534f15adfd3f98df1dadee9e7a3d46d092
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-03-26 12:58:58 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdarg.h>
 
-int main() {
+int average(int count, ...) {
+    va_list args;         
+    va_start(args, count);  
 
+    int sum = 0;
+    for (int i = 0; i < count; i++) {
+        sum += va_arg(args, int);
+    }
+
+    va_end(args); 
+
+    if (count == 0) return 0; 
+    return sum / count;
+}
+
+int main(){
+
+     printf("%d\n", average(3, 10, 20, 30));
+     printf("%d\n", average(5, 10, 20, 30, 40, 50));
+  
     return 0;
 }
