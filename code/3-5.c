1bd8d988788be196dee406db770f76bd1bd7bbe2
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-03-26 13:10:20 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int main() {
+void my_sum(char type, int count, ...) {
+    va_list args;
+    va_start(args, count);
 
-    return 0;
+    if (type == 'S') {      
+        for (int i = 0; i < count; i++) {
+            printf("%s", va_arg(args, char*));
+        }
+        printf("\n");
+    } else if (type == 'C') { 
+        for (int i = 0; i < count; i++) {
+            printf("%c", va_arg(args, int)); 
+        }
+        printf("\n");
+    } else if (type == 'D') { 
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
+            sum += va_arg(args, int);
+        }
+        printf("%d\n", sum);
+    }
+
+    va_end(args);
+}
+
+int main(){
+   my_sum('S', 2, "Hello", "World"); 
+   my_sum('C', 3, 'C', 'N', 'U');    
+   my_sum('D', 4, 10, 20, 30, 40);   
+
+   return 0;
 }
